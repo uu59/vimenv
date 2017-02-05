b9ff8eb64f3b8bb411bb1948100dfd4f3707f323
uu59 <k@uu59.org>
2017-02-05 15:54:31 +0900

    CSV to markdown table <-> markdown table to CSV

--- csv-md-table.rb
+++ csv-md-table.rb
@@ -1,36 +1,72 @@
 require "csv"
 
-csv = STDIN.read
-output = ""
-col_width = {}
-rows = []
 
 def display_width(str)
   return 0 unless str
   str.chars.map{|c| c.ascii_only? ? 1 : 2 }.inject(:+)
 end
 
-CSV.parse(csv, col_sep: ARGV.first || ",") do |row|
-  row.each.with_index do |col, i|
-    col_width[i] = [col_width[i], display_width(col)].compact.max
+def md_to_csv(str)
+  output = ""
+  str.lines.each_with_index do |row, i|
+    next if i == 1 # cols = "| ----- | -- |"
+    _, *cols, _ = row.split("|")
+    output << cols.map(&:strip).join("\t")
+    output << "\n"
   end
-  rows << row
+  output
 end
-max_width = col_width.max
 
-rows.each.with_index do |row, ri|
-  row.each.with_index do |col, ci|
-    w = display_width(col)
-    mw = col_width[ci]
-    output << "| #{col}#{" " * (mw - w)} "
+def csv_to_md(csv)
+  output = ""
+  col_width = {}
+  rows = []
+  col_sep = (csv.include?("\t") ? "\t" : ",")
+
+  CSV.parse(csv, col_sep: col_sep) do |row|
+    row.each.with_index do |col, i|
+      col_width[i] = [col_width[i], display_width(col)].compact.max
+    end
+    rows << row
   end
-  output << "|\n"
-  if ri.zero?
-    row.length.times.with_index do |_, i|
-      output << "| #{"-" * col_width[i]} "
+  max_width = col_width.max
+
+  rows.each.with_index do |row, ri|
+    row.each.with_index do |col, ci|
+      w = display_width(col)
+      mw = col_width[ci]
+      output << "| #{col}#{" " * (mw - w)} "
     end
     output << "|\n"
+    if ri.zero?
+      row.length.times.with_index do |_, i|
+        output << "| #{"-" * col_width[i]} "
+      end
+      output << "|\n"
+    end
   end
+  output
 end
 
+body = STDIN.read
+output =
+  if body.lines.find{|line| line.match(/^[\|\- ]*$/)}
+    md_to_csv(body)
+  else
+    csv_to_md(body)
+  end
+
 puts output
+
+
+__END__
+col1,col2
+ああ,e
+いうあいうあ,e
+
+col1,col2
+ああ,e
+いうあいうあ,e
+
+
+
